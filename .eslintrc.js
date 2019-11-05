104dd6959921476f87998d2bc3fc88471b0c32d6
Juliane Silva <jl.sansil@gmail.com>
2019-11-05 01:22:27 -0300

    chore: configuração do ESLint e do Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-native'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
