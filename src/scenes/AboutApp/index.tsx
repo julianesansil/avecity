ab630c95d6378b984eff85ff7ef3c837a19eb9c8
Juliane Silva <jl.sansil@gmail.com>
2019-11-08 23:40:27 -0300

    feat: implementação do Sobre o Aplicativo

--- src/scenes/AboutApp/index.tsx
+++ src/scenes/AboutApp/index.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Linking } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function AboutApp() {
   return (
     <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Sobre o app</Text>
+      <Text>Aplicativo para mapeamento de localidades em cidades.</Text>
+
+      <Text>Desenvolvido por:</Text>
+      <Text>Juliane Silva</Text>
+
+      <Text>Celular/Whatsapp:</Text>
+      <TouchableWithoutFeedback
+        onPress={() => Linking.openURL('tel:11951545438')}>
+        <Text>(11) 95154-5438</Text>
+      </TouchableWithoutFeedback>
+
+      <Text>E-mail:</Text>
+      <TouchableWithoutFeedback
+        onPress={() => Linking.openURL('mailto:jl.sansil@gmail.com')}>
+        <Text>jl.sansil@gmail.com</Text>
+      </TouchableWithoutFeedback>
+
+      <Text>Linkedin:</Text>
+      <TouchableWithoutFeedback
+        onPress={() =>
+          Linking.openURL('https://www.linkedin.com/in/jlsansil/')
+        }>
+        <Text>https://www.linkedin.com/in/jlsansil/</Text>
+      </TouchableWithoutFeedback>
+
+      <Text>Github:</Text>
+      <TouchableWithoutFeedback
+        onPress={() => Linking.openURL('https://github.com/julianesansil')}>
+        <Text>https://github.com/julianesansil</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
 
 AboutApp.navigationOptions = {
-  title: 'Sobre o Aplicativo',
+  title: 'Sobre',
 };
 
 export default AboutApp;
